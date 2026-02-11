68069c1b00f26d559d17d46b046b1d3a1b97fd67
FormaOS Deployment <deploy@formaos.com>
2026-02-12 10:20:12 +1030

    Remove global scroll indicator component

--- app/(marketing)/security/SecurityPageContentNew.tsx
+++ app/(marketing)/security/SecurityPageContentNew.tsx
@@ -217,21 +217,6 @@ function SecurityHero() {
         </motion.div>
       </motion.div>
 
-      {/* Scroll Indicator */}
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1.5, duration: 1 }}
-        className="absolute bottom-8 left-1/2 -translate-x-1/2"
-      >
-        <motion.div
-          animate={{ y: [0, 8, 0] }}
-          transition={{ duration: 2, repeat: Infinity, ease: 'easeInOut' }}
-          className="w-6 h-10 rounded-full border-2 border-white/20 flex items-start justify-center p-2"
-        >
-          <motion.div className="w-1 h-2 rounded-full bg-red-400" />
-        </motion.div>
-      </motion.div>
     </section>
   );
 }
